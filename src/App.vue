b9250e5a546582ec2ea7a60306f18a776b51edbf
abethel912 <abethel180@gmail.com>
2023-03-14 21:03:18 -0400

    Input login and cart buttons, working on basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,49 @@
-
-
 <template>
-  <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div id="wrapper">
+      <nav class="navbar is-dark">
+        <div class="navbar-brand">
+          <router-link to="/" class="navbar-item"><strong>Amanda's Jackets</strong></router-link>
+          <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+          <router-link to="/winter" class="navbar-item">Winter</router-link>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/login" class="btn btn-light">Login</router-link>
+
+              <router-link to="/cart" class="btn btn-success">
+              <span class="icon"><i class="bi bi-cart"></i></span>
+              <span>Cart</span>
+            </router-link>
+            </div>
+          </div>
+        </div> 
+        </div>
       </nav>
-    </div>
-  </header>
+        
+      <section class="section">
+        <router-view/>
+      </section>
 
-  <RouterView />
-</template>
+      <footer class="footer">
+        <p class="text-center"> Made by:Amanda Bethel </p>
+      </footer>
+      
+    </div>
+    </template>
+        
+
 
 <style>
-@import '../node_modules/bootstrap'
+@import '../node_modules/bootstrap';
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div class="home">
+    Home
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Home',
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
